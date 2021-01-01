f506afa83e910bf9aa7aac895783a66c3f8cab2e
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-01 18:31:52 +0200

    fake users
    Signed-off-by: Gary Berkinfand <gberkinfand@gmail.com>

--- earthshatter/app/views.py
+++ earthshatter/app/views.py
@@ -7,7 +7,6 @@ def index(request):
     return render(request, 'app/index.html')
 
 def users(request):
-
     user_list = User.objects.order_by('first_name')
     user_dict = {"users": user_list}
     return render(request, 'app/users.html', context=user_dict)
