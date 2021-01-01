e369499f214578601588ef954810b90bcabe48b7
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-01 20:45:02 +0200

    static
    Signed-off-by: Gary Berkinfand <gberkinfand@gmail.com>

--- earthshatter/earthshatter/settings.py
+++ earthshatter/earthshatter/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'earthshatter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
+        'DIRS': [TEMPLATE_DIR, STATIC_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
