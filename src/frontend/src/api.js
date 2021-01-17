b7b83aeff1ef5af0069200ab01dbcc7ab1f09069
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-17 18:10:32 +0200

    Removing old src

--- src/frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export const groupComments = () => {
-  return fetch("http...").then((re) => re.json());
-};
-export const myComments = () => {
-  return fetch("https://jsonplaceholder.typicode.com/posts").then((re) =>
-    re.json()
-  );
-};
