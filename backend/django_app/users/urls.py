5176e7e1b61107173e47b936caff59bd4b2816b0
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-19 23:16:58 +0200

    Adding tests dir

--- backend/django_app/users/urls.py
+++ backend/django_app/users/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
     path('me/', ManageUserView.as_view(), name='me'),
-    path('users/', ListUserView.as_view(), name='users') # TODO fix list all viewers (populate)
+    path('users/', ListUserView.as_view(), name='users')
 ]
 
 
