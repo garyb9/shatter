5578427326577d7413ed5a7a94381554cab81221
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-20 18:49:12 +0200

    Adding boards urls

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -46,12 +46,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    #Tools
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
     'corsheaders',
 
+    # Apps
     'users',
     'boards',
 ]
