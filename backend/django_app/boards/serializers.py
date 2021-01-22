c3ed338ff0537daebc59d66f877b02d815503451
Gary Berkinfand <gberkinfand@gmail.com>
2021-01-22 17:27:55 +0200

    Adding nested routing

--- backend/django_app/boards/serializers.py
+++ backend/django_app/boards/serializers.py
@@ -25,21 +25,30 @@ class BoardSerializer(serializers.ModelSerializer):
         )
         read_only_Fields = ('id',)
         # extra_kwargs = {'id':{'read_only':True,}}
+    
+    # def create(self, validated_data):
+    #     """Create a new user with encrypted password and return it"""
+    #     return get_user_model().objects.create_user(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     """Update a user, setting the password correctly and return it"""
+    #     password = validated_data.pop('password', None)
+    #     user = super().update(instance, validated_data)
+
+    #     if password:
+    #         user.set_password(password)
+    #         user.save()
+            
+    #     return user
 
 
 
 class ThreadSerializer(serializers.ModelSerializer):
     """Serializer for thread object"""
 
-    posts = serializers.PrimaryKeyRelatedField(
-        many=True,
-        read_only=True
-    )
-
-    replies = serializers.PrimaryKeyRelatedField(
-        many=True,
-        read_only=True
-    )
+    posts       = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    replies     = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    replies_to  = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Thread
