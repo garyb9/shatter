7cb8059439c545631af145330f513257786b8a34
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-09 22:30:50 +0200

    adding initial nft app

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     # Apps
     'users',
     'boards',
+    'nft',
 ]
 
 MIDDLEWARE = [
---
+++ backend/django_app/nft/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NftConfig(AppConfig):
+    name = 'nft'
