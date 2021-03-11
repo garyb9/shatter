4d5a40df178c324e230a6192f27d8018367a9abd
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-11 18:05:42 +0200

    Web3 latest block

--- backend/django_app/scripts/Web3Connect.py
+++ backend/django_app/scripts/Web3Connect.py
@@ -1,6 +1,7 @@
-
 import sys
+from hexbytes.main import HexBytes
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 
 rinkebyWS = "wss://rinkeby-light.eth.linkpool.io/ws"
 
@@ -13,7 +14,15 @@ if not w3.isConnected():
     sys.exit(1)
 
 print("Web3 is Connected")
-# TODO do something
+
+# inject the poa compatibility middleware to the innermost layer
+w3.middleware_onion.inject(geth_poa_middleware, layer=0) 
+latest_block = w3.eth.get_block('latest')
+for key in latest_block:
+    if isinstance(latest_block[key], bytes):
+        print(key + ": " + str(latest_block[key].hex()))
+    else:
+        print(key + ": " + str(latest_block[key]))
 sys.exit(0)
 
     
