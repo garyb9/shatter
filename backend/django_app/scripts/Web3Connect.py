b617cd7a434dc691d9f85f76d76d9e3d18cfc0fe
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-11 17:41:15 +0200

    Adding Web3 connection script

--- backend/django_app/scripts/Web3Connect.py
+++ backend/django_app/scripts/Web3Connect.py
@@ -8,9 +8,14 @@ print("----- Initiating connection to Ethereum Rinkeby testnet -----")
 print("Using Websocket endpoint - " + rinkebyWS)
 w3 = Web3(Web3.WebsocketProvider(endpoint_uri=rinkebyWS, websocket_timeout=60))
 
-if w3.isConnected():
-    print("Web3 is Connected")
-else:
+if not w3.isConnected():
     print("Connection failed")
     sys.exit(1)
+
+print("Web3 is Connected")
+
+
+sys.exit(0)
+
+    
     
