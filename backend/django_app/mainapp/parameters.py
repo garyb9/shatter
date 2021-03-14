010a6a6cd4a81d5d6d157586e24beeb10feaa588
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-14 14:36:31 +0200

    Adding Infura websocket and HTTP addresses

--- backend/django_app/mainapp/parameters.py
+++ backend/django_app/mainapp/parameters.py
@@ -92,4 +92,10 @@ VERSATILEIMAGEFIELD_RENDITION_KEY_SETS = {
 # --------------------- ETH Params ---------------------
 # ------------------------------------------------------
 
-OPENSEA_URL = "https://api.opensea.io/api/v1/assets"
+MAINNET_INFURA_HTTP     = "https://mainnet.infura.io/v3/"
+
+MAINNET_INFURA_WS       = "wss://mainnet.infura.io/ws/v3/"
+
+RINKEBY_WS              = "wss://rinkeby-light.eth.linkpool.io/ws"
+
+OPENSEA_URL             = "https://api.opensea.io/api/v1/assets"
