d0d6a1773c4b79c7fff9f168098dfe037fec1b89
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-22 00:01:21 +0200

    adding ERC20, ERC721, ERC1155 models

--- backend/django_app/blockchain/admin.py
+++ backend/django_app/blockchain/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import gettext as _
-from core.models import Wallet
+from core.models import Wallet, ERC20, ERC721, ERC1155
 
 class WalletAdmin(admin.ModelAdmin):
     """Wallet model admin"""
@@ -9,10 +9,48 @@ class WalletAdmin(admin.ModelAdmin):
     list_display = ['id',]
     fieldsets = (
         (_('Base Info'), {'fields': ('id',)}),
-        # (_('User Info'), {'fields': ('user',)}),
-        (_('Wallet Info'),{'fields': ('wallet_address',)}),
+        (_('Wallet Info'),{'fields': ('address',)}),
     )
     
 
 
-admin.site.register(Wallet, WalletAdmin)
+class ERC20Admin(admin.ModelAdmin):
+    """ERC20 model admin"""
+    
+    ordering = ['id']
+    list_display = ['id',]
+    fieldsets = (
+        (_('Base Info'), {'fields': ('id',)}),
+        (_('Wallet Info'),{'fields': ('wallet',)}),
+    )
+
+
+class ERC721Admin(admin.ModelAdmin):
+    """ERC20 model admin"""
+    
+    ordering = ['id']
+    list_display = ['id',]
+    fieldsets = (
+        (_('Base Info'), {'fields': ('id',)}),
+        (_('Wallet Info'),{'fields': ('wallet',)}),
+    )
+
+
+class ERC1155Admin(admin.ModelAdmin):
+    """ERC20 model admin"""
+    
+    ordering = ['id']
+    list_display = ['id',]
+    fieldsets = (
+        (_('Base Info'), {'fields': ('id',)}),
+        (_('Wallet Info'),{'fields': ('wallet',)}),
+    )
+    
+
+
+# Register Admins
+
+admin.site.register(Wallet,     WalletAdmin)
+admin.site.register(ERC20,      ERC20Admin)
+admin.site.register(ERC721,     ERC721Admin)
+admin.site.register(ERC1155,    ERC1155Admin)
--- backend/django_app/core/models_blockchain.py
+++ backend/django_app/core/models_blockchain.py
@@ -3,6 +3,8 @@ import requests
 import threading
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 
@@ -23,7 +25,19 @@ class WalletManager(models.Manager):
     """ Represents a basic Wallet Model Manager."""
     
     def create_wallet(self, **validated_data):
-        pass
+        if 'address' not in validated_data:
+            raise ValidationError(message="No 'address' provided -> please provide an 'address'.")
+        address = validated_data["address"]
+
+        mutex = threading.Lock()
+        threads = []
+        # for dateCode in dateCodes:
+        #     t = threading.Thread(target=self.ReqThread, args=(dateCode, mutex))
+        #     threads.append(t)
+        #     t.start()
+
+        for t in threads:
+            t.join()
 
 
 class Wallet(models.Model):
@@ -31,22 +45,48 @@ class Wallet(models.Model):
 
     id                  = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, verbose_name=_('Unique ID')) 
     user                = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='wallet', blank=True, null=True, on_delete=models.CASCADE, verbose_name=_('User'))
-    wallet_address      = models.CharField(default=None, max_length=255, verbose_name=_('Wallet Address'))
+    address             = models.CharField(default=None, max_length=255, verbose_name=_('Wallet Address'))
 
     objects             = WalletManager()
 
     def __str__(self):
         return str(self.id)
 
-    # class Meta:
-    #     abstract = True
 
 
 
 # -------------------------------------------------
+# ---------------------- ERC20 --------------------
+# -------------------------------------------------
+class ERC20Manager(models.Manager):
+    """ Represents a basic ERC20 Model Manager."""
+    
+    def request_token(self):
+        querystring = {
+            "order_direction":"desc",
+            "offset":"0",
+            "limit":"20"
+        }
+        response = requests.request("GET", OPENSEA_API_URL, params=querystring)
+        print(response.text)
+
+
+class ERC20(models.Model):
+    """ Represents a basic ERC20 Model. """
+
+    id      = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, verbose_name=_('Unique ID'))
+    wallet  = models.ForeignKey(Wallet, related_name='erc20', blank=True, null=True, on_delete=models.CASCADE, verbose_name=_('Wallet'))
+
+    objects = ERC20Manager()
+
+    def __str__(self):
+        return str(id)
+
+
+# ----------------------------------------------------
 # ---------------------- ERC721 ----------------------
-# -------------------------------------------------
-class ERC721ModelManager(models.Manager):
+# ----------------------------------------------------
+class ERC721Manager(models.Manager):
     """ Represents a basic ERC721 Model Manager."""
     
     def request_token(self):
@@ -59,10 +99,11 @@ class ERC721ModelManager(models.Manager):
         print(response.text)
 
 
-class ERC721Model(models.Model):
+class ERC721(models.Model):
     """ Represents a basic ERC721 Model. """
 
-    id      = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, verbose_name=_('Unique ID')) 
+    id      = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, verbose_name=_('Unique ID'))
+    wallet  = models.ForeignKey(Wallet, related_name='erc721', blank=True, null=True, on_delete=models.CASCADE, verbose_name=_('Wallet'))
     name    = models.CharField(default=None, max_length=255, verbose_name=_('Name'))
 
     # token_id              - The token ID of the ERC721 asset
@@ -75,11 +116,37 @@ class ERC721Model(models.Model):
     # traits                - A list of traits associated with the item (see traits section) -> json = JSONField()
     # last_sale             - When this item was last sold (null if there was no last sale)
 
-    objects = ERC721ModelManager()
+    objects = ERC721Manager()
 
     def __str__(self):
         return str(id)
 
-    class Meta:
-        abstract = True
+# -------------------------------------------------
+# ---------------------- ERC721 ----------------------
+# -------------------------------------------------
+class ERC1155Manager(models.Manager):
+    """ Represents a basic ERC1155 Model Manager."""
+    
+    def request_token(self):
+        querystring = {
+            "order_direction":"desc",
+            "offset":"0",
+            "limit":"20"
+        }
+        response = requests.request("GET", OPENSEA_API_URL, params=querystring)
+        print(response.text)
+
+
+class ERC1155(models.Model):
+    """ Represents a basic ERC1155 Model. """
+
+    id      = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, verbose_name=_('Unique ID'))
+    wallet  = models.ForeignKey(Wallet, related_name='erc1155', blank=True, null=True, on_delete=models.CASCADE, verbose_name=_('Wallet'))
+    name    = models.CharField(default=None, max_length=255, verbose_name=_('Name'))
+
+    objects = ERC1155Manager()
+
+    def __str__(self):
+        return str(id)
+
     
