489378fcd223e9879392a77081ccebf21d879446
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-21 12:43:44 +0200

    fixing Opensea script with API and formatting

--- backend/django_app/scripts/generate_eth_address.py
+++ backend/django_app/scripts/generate_eth_address.py
@@ -4,9 +4,9 @@ from sha3 import keccak_256
 
 def generate():
     gen = {}
-    gen["private_key"] = keccak_256(token_bytes(32)).digest()
-    gen["public_key"] = PublicKey.from_valid_secret(gen["private_key"]).format(compressed=False)[1:]
-    gen["address"] = keccak_256(gen["public_key"]).digest()[-20:]
+    gen["private_key"]  = keccak_256(token_bytes(32)).digest()
+    gen["public_key"]   = PublicKey.from_valid_secret(gen["private_key"]).format(compressed=False)[1:]
+    gen["address"]      = keccak_256(gen["public_key"]).digest()[-20:]
     return gen
 
 if __name__ == "__main__":
--- backend/django_app/scripts/web3_opensea_query.py
+++ backend/django_app/scripts/web3_opensea_query.py
@@ -14,12 +14,16 @@ if not hasattr(settings, 'ETH_ADDRESS_LIST'):
 
 ETH_ADDRESS_LIST = settings.ETH_ADDRESS_LIST
 
-
 OPENSEA_API_URL = settings.OPENSEA_API_URL
+
+if hasattr(settings, 'OPENSEA_API_KEY'):
+    OPENSEA_API_KEY = settings.OPENSEA_API_KEY
+else:
+    OPENSEA_API_KEY = ""
 
 import json
 import requests
-url = "https://api.opensea.io/api/v1/assets"
+url = "https://api.opensea.io/api/v1/assets" + "?X-API-KEY=" + OPENSEA_API_KEY
 querystring = {
     "owner":ETH_ADDRESS_LIST[0],
     "order_direction":"desc",
@@ -27,6 +31,16 @@ querystring = {
     "limit":"20"
     }
 
+print("Querying Opensea =>")
+print("URL: " + url)
+print("Address: " + ETH_ADDRESS_LIST[0])
 response = requests.request("GET", url, params=querystring)
 
-print(response.text)
+print("Response =>")
+respJSON = response.json()
+for key in respJSON:
+    print("Key: -----> ", key)
+    for item in respJSON[key]:
+        print(json.dumps(item, indent = 4))
+
+
