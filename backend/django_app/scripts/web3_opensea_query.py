6a733554a8144b35b7f88e1cd727f20070e8950b
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-21 23:58:52 +0200

    updating opensea query

--- backend/django_app/scripts/web3_opensea_query.py
+++ backend/django_app/scripts/web3_opensea_query.py
@@ -23,9 +23,12 @@ else:
 
 import json
 import requests
+
+addressIndex = 0
+
 url = "https://api.opensea.io/api/v1/assets" + "?X-API-KEY=" + OPENSEA_API_KEY
 querystring = {
-    "owner":ETH_ADDRESS_LIST[0],
+    "owner":ETH_ADDRESS_LIST[addressIndex],
     "order_direction":"desc",
     "offset":"0",
     "limit":"20"
@@ -33,14 +36,24 @@ querystring = {
 
 print("Querying Opensea =>")
 print("URL: " + url)
-print("Address: " + ETH_ADDRESS_LIST[0])
+print("Address: " + ETH_ADDRESS_LIST[addressIndex])
 response = requests.request("GET", url, params=querystring)
 
-print("Response =>")
-respJSON = response.json()
-for key in respJSON:
-    print("Key: -----> ", key)
-    for item in respJSON[key]:
-        print(json.dumps(item, indent = 4))
+dumpFile = "scripts_dump.txt"
+print("Writing response to =>", dumpFile)
+
+with open(dumpFile, "w") as f:
+    f.write("URL: " + url + '\n')
+    f.write("Address: " + ETH_ADDRESS_LIST[addressIndex] + '\n')
+    respJSON = response.json()
+    for key in respJSON:
+        itemCount = 1
+        f.write("Key: -----> " + key + '\n')   
+        for item in respJSON[key]:
+            f.write("-"*20 + ' ' + str(itemCount) + ' ' + "-"*20 + '\n')
+            f.write(json.dumps(item, indent = 4) + '\n')
+            itemCount += 1
+print("Done")
+
 
 
