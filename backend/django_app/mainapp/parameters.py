3ea53b54b813fe886546ef6229e06b4120076195
Gary Berkinfand <gberkinfand@gmail.com>
2021-03-29 22:04:16 +0300

    params update

--- backend/django_app/mainapp/parameters.py
+++ backend/django_app/mainapp/parameters.py
@@ -105,3 +105,7 @@ RINKEBY_WS              = "wss://rinkeby-light.eth.linkpool.io/ws"
 OPENSEA_API_URL         = "https://api.opensea.io/api/v1/assets"
 
 LIMIT_OPENSEA           = "30"  # default is 20, caps at 50
+
+ALCHEMY_API_HTTP        = "https://eth-mainnet.alchemyapi.io/v2/"
+
+ALCHEMY_API_WS          = "wss://eth-mainnet.ws.alchemyapi.io/v2/"
